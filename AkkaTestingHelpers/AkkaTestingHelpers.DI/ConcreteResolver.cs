2c1d446e64ed136c95474bdab385ac814b4e8e99
Connel Hooley <connelhooley@gmail.com>
2017-05-28 08:45:19 +0100

    Removed ForwardMessage

--- AkkaTestingHelpers/AkkaTestingHelpers.DI/ConcreteResolver.cs
+++ AkkaTestingHelpers/AkkaTestingHelpers.DI/ConcreteResolver.cs
@@ -57,27 +57,20 @@ namespace ConnelHooley.AkkaTestingHelpers.DI
                 expectedChildrenCount,
                 supervisor);
         
-        public void TellMessage<TMessage>(IActorRef recipient, TMessage message, int expectedChildrenCount)
+        public void TellMessage<TMessage>(IActorRef recipient, TMessage message, int waitForChildrenCount)
         {
-            _childWaiter.Start(_testKit, expectedChildrenCount);
+            _childWaiter.Start(_testKit, waitForChildrenCount);
             recipient.Tell(message);
             _childWaiter.Wait();
         }
 
-        public void TellMessage<TMessage>(IActorRef recipient, TMessage message, IActorRef sender, int expectedChildrenCount)
+        public void TellMessage<TMessage>(IActorRef recipient, TMessage message, IActorRef sender, int waitForChildrenCount)
         {
-            _childWaiter.Start(_testKit, expectedChildrenCount);
+            _childWaiter.Start(_testKit, waitForChildrenCount);
             recipient.Tell(message, sender);
             _childWaiter.Wait();
         }
-        
-        public void ForwardMessage<TMessage>(IActorRef recipient, TMessage message, int expectedChildrenCount)
-        {
-            _childWaiter.Start(_testKit, expectedChildrenCount);
-            recipient.Forward(message);
-            _childWaiter.Wait();
-        }
-        
+
         private ActorBase Resolve(Type actorType)
         {
             if (!_factories.ContainsKey(actorType))
