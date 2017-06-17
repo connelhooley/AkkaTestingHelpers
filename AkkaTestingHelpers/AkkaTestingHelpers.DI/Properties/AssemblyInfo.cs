2074feb6e5214b598b4364f4fdcf3b934478ecd3
Connel Hooley <connelhooley@gmail.com>
2017-06-17 16:38:19 +0100

    Added nuget info into assembly info

--- AkkaTestingHelpers/AkkaTestingHelpers.DI/Properties/AssemblyInfo.cs
+++ AkkaTestingHelpers/AkkaTestingHelpers.DI/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using NullGuard;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AkkaTestingHelpers.DI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Akka TestKit Dependancy Injection Helpers")]
+[assembly: AssemblyDescription("Akka DependencyResolver that mocks child Actors into Stubs or TestProbes.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AkkaTestingHelpers.DI")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Connel Hooley")]
+[assembly: AssemblyProduct("ConnelHooley.AkkaTestingHelpers.DI")]
+[assembly: AssemblyCopyright("Copyright Connel Hooley 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,10 +32,9 @@ using NullGuard;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 
 [assembly: NullGuard(ValidationFlags.All)]
 [assembly: InternalsVisibleTo("ConnelHooley.AkkaTestingHelpers.DI.SmallTests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
