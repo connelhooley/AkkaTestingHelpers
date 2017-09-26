f16ce59c689cbeb87f8e9f66576ebb689c231b2c
Connel Hooley <connelhooley@gmail.com>
2017-09-26 21:37:45 +0100

    tweaked timefactor back

--- AkkaTestingHelpers/AkkaTestingHelpers.DI.MediumTests/AkkaConfig.cs
+++ AkkaTestingHelpers/AkkaTestingHelpers.DI.MediumTests/AkkaConfig.cs
@@ -15,7 +15,7 @@ namespace ConnelHooley.AkkaTestingHelpers.DI.MediumTests
                     SingleExpectDefault = 100,
                     DefaultTimeout = 100,
 #if DEBUG
-                    Timefactor = 10,
+                    Timefactor = 1,
 #else
                     Timefactor = 10,
 #endif
