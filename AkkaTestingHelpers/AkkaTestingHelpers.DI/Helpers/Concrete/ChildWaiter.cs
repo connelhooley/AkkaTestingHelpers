849f451771b1e92e5276e1cf66b5a436f9610a1f
Connel Hooley <connelhooley@gmail.com>
2017-09-27 16:36:28 +0100

    Removed WriteLines used for debugging

--- AkkaTestingHelpers/AkkaTestingHelpers.DI/Helpers/Concrete/ChildWaiter.cs
+++ AkkaTestingHelpers/AkkaTestingHelpers.DI/Helpers/Concrete/ChildWaiter.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using Akka.TestKit;
 using ConnelHooley.AkkaTestingHelpers.DI.Helpers.Abstract;
 
@@ -12,34 +11,25 @@ namespace ConnelHooley.AkkaTestingHelpers.DI.Helpers.Concrete
         
         public void Start(TestKitBase testKit, int expectedChildrenCount)
         {
-            Console.WriteLine("Waiting for AutoResetEvent");
             if (_waitingToStart.WaitOne())
             {
-                var x = expectedChildrenCount < 0 ? 0 : expectedChildrenCount;
-                Console.WriteLine($"Creating test latch {x}");
                 _waitForChildren = testKit.CreateTestLatch(
                     expectedChildrenCount < 0 
                         ? 0 
                         : expectedChildrenCount);
             }
-            Console.WriteLine("Leaving start");
         }
 
         public void Wait()
         {
-            Console.WriteLine("Calling ready on test latch");
             _waitForChildren?.Ready();
-            Console.WriteLine("Called ready on test latch");
             _waitForChildren = null;
             _waitingToStart.Set();
-            Console.WriteLine("Leaving wait");
         }
 
         public void ResolvedChild()
         {
-            Console.WriteLine("Calling count down test latch");
             _waitForChildren?.CountDown();
-            Console.WriteLine("Leaving resolve child");
         }
     }
 }
