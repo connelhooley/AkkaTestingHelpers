34e0ab221ce567301510321b9db006de3cdc4014
Connel Hooley <connelhooley@gmail.com>
2018-10-02 12:13:21 +0100

    UnhandledExceptions unit tests

---
+++ AkkaTestingHelpers.SmallTests/UnitTestFrameworkTests/UnhandledExceptions.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+using System.Collections.Generic;
+using System;
+
+namespace ConnelHooley.AkkaTestingHelpers.SmallTests.UnitTestFrameworkTests
+{
+    public class UnhandledExceptions : TestBase
+    {
+        [Fact]
+        public void UnitTestFramework_UnhandledExceptions_ReturnsUnhandledExceptions()
+        {
+            //arrange
+            UnitTestFramework<DummyActor> sut = CreateUnitTestFramework();
+
+            //act
+            IEnumerable<Exception> result = sut.UnhandledExceptions;
+
+            //assert
+            result.Should().BeSameAs(TestProbeParentActorUnhandledExceptions);
+        }
+    }
+}
